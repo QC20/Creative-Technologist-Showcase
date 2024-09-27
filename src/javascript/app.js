20d63010b964c2cf4af52ec06fe310bb1e4dd275
QC20 <xgv866@alumni.ku.dk>
2024-09-27 14:20:15 +0200

    updates to files

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -4,7 +4,6 @@ var Engine = Matter.Engine,
     World = Matter.World,
     Body = Matter.Body,
     Bodies = Matter.Bodies;
-    
 
 // Initialize variables
 var engine;
@@ -17,20 +16,9 @@ var blocks = [];
 var hBlocks = document.getElementsByClassName("anarchy");
 var pageWidth = 0;
 
-// bodies
-var blocks = [];
-var walls = [];
-var ground;
-var ceiling;
-
-// DOM elements
-var hBlocks = document.getElementsByClassName("anarchy");
-
-var pageWidth = 0;
-
 window.onload = function() {
   pageWidth = window.innerWidth;
-/*
+  /*
   // Request access to the webcam
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function(stream) {
@@ -46,7 +34,7 @@ window.onload = function() {
     .catch(function(err) {
       console.log("An error occurred: " + err);
     });
-    */
+  */
 };
 
 window.onresize = function() {
@@ -65,8 +53,8 @@ function Box(x, y, w, h) {
     friction: 0.5,
     restitution: 0.7,
     collisionFilter: {
-      category: 0x0001,
-      mask: 0x0001
+      category: 0x0002,
+      mask: 0x0003
     }
   };
   this.body = Bodies.rectangle(x, y, w, h, options);
@@ -81,8 +69,8 @@ function Ball(x, y, r) {
     friction: 0.5,
     restitution: 0.7,
     collisionFilter: {
-      category: 0x0001,
-      mask: 0x0001
+      category: 0x0002,
+      mask: 0x0003
     }
   };
   this.body = Bodies.circle(x, y, r, options);
@@ -90,8 +78,6 @@ function Ball(x, y, r) {
   Body.setVelocity(this.body, { x: xVel, y: 0 });
   World.add(engine.world, [this.body]);
 }
-
-
 
 function setup() {
   engine = Engine.create();
@@ -139,10 +125,34 @@ function setup() {
     }
   }
 
-  ground = Bodies.rectangle(10000, -50, 20000, 100, { isStatic: true });
-  ceiling = Bodies.rectangle(10000, 40050, 20000, 100, { isStatic: true });
-  walls[0] = Bodies.rectangle(-50, 20000, 100, 40000, { isStatic: true });
-  walls[1] = Bodies.rectangle(window.innerWidth + 50, 20000, 100, 40000, { isStatic: true });
+  ground = Bodies.rectangle(10000, -50, 20000, 100, { 
+    isStatic: true,
+    collisionFilter: {
+      category: 0x0001,
+      mask: 0x0002
+    }
+  });
+  ceiling = Bodies.rectangle(10000, 40050, 20000, 100, { 
+    isStatic: true,
+    collisionFilter: {
+      category: 0x0001,
+      mask: 0x0002
+    }
+  });
+  walls[0] = Bodies.rectangle(-50, 20000, 100, 40000, { 
+    isStatic: true,
+    collisionFilter: {
+      category: 0x0001,
+      mask: 0x0002
+    }
+  });
+  walls[1] = Bodies.rectangle(window.innerWidth + 50, 20000, 100, 40000, { 
+    isStatic: true,
+    collisionFilter: {
+      category: 0x0001,
+      mask: 0x0002
+    }
+  });
   
   World.add(engine.world, [ground, ceiling, walls[0], walls[1]]);
 }
@@ -165,4 +175,3 @@ draw();
   }
   window.requestAnimationFrame(render);
 })();
-
